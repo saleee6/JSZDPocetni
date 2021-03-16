e7e9b01e165fd4fcf0c554f350ab67566ce900a8
saleee6 <58145119+saleee6@users.noreply.github.com>
2021-03-16 16:33:51 +0100

    Dodato generisanje Entity klase

--- initial.py
+++ initial.py
@@ -1,11 +1,12 @@
-from __future__ import unicode_literals
-import os
-from os.path import dirname, join
+from os import mkdir
+from os.path import exists, dirname, join
+import jinja2
 from textx import metamodel_from_file
-from textx.export import metamodel_export, model_export
+from datetime import datetime
 
 this_folder = dirname(__file__)
 grammar_path = join(this_folder, 'grammar.tx')
+template_folder = join(this_folder, 'templates')
 
 class SimpleType(object):
     def __init__(self, parent, name):
@@ -37,19 +38,51 @@ def main(debug=False):
 
     entity_mm = get_entity_mm(debug)
 
+    def javatype(s):
+        """
+        Maps type names from SimpleType to Java.
+        """
+        return {
+                'int': 'int',
+                'string': 'String'
+        }.get(s.name, s.name)
+
+    # Create the output folder
+    srcgen_folder = join(this_folder, 'generated')
+    if not exists(srcgen_folder):
+        mkdir(srcgen_folder)
+
+    # Initialize the template engine.
+    jinja_env = jinja2.Environment(
+        loader=jinja2.FileSystemLoader(template_folder),
+        trim_blocks=True,
+        lstrip_blocks=True)
+
+    # Register the filter for mapping Entity type names to Java type names.
+    jinja_env.filters['javatype'] = javatype
+
+    # Load the Java template
+    template = jinja_env.get_template('entity.template')
+
     # Export to .dot file for visualization
-    dot_folder = join(this_folder, 'dotexport')
-    if not os.path.exists(dot_folder):
-        os.mkdir(dot_folder)
-    metamodel_export(entity_mm, join(dot_folder, 'grammar_meta.dot'))
+    #dot_folder = join(this_folder, 'dotexport')
+    #if not os.path.exists(dot_folder):
+    #    os.mkdir(dot_folder)
+    #metamodel_export(entity_mm, join(dot_folder, 'grammar_meta.dot'))
 
     # Izgradnja modela u odnosu na konkretan primer
-    example_model = entity_mm.model_from_file(join(this_folder, 'example.jszd'))
-
-    print(example_model.entities)
+    entity_model = entity_mm.model_from_file(join(this_folder, 'example.jszd'))
 
     # Kreiranje .dot fajla u odnosu na izgradjeni model
-    model_export(example_model, join(dot_folder, 'example.dot'))
+    #model_export(example_model, join(dot_folder, 'example.dot'))
+
+    # Generate Java code
+    dt_string = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+    for entity in entity_model.entities:
+        # For each entity generate java file
+        with open(join(srcgen_folder,
+                      "%s.java" % entity.name.capitalize()), 'w') as f:
+            f.write(template.render(entity=entity, time=dt_string))
 
 
 if __name__ == "__main__":
