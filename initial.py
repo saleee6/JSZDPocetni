92ecc447b1d01decf6118459e1044e9ad9d6ebf6
Aleksa Ivkovic <58144983+AleksaIvkovic@users.noreply.github.com>
2021-03-17 23:27:29 +0100

    Templates created
    Controller, Interface, Repository and Service.
    
    Entity template edited

--- initial.py
+++ initial.py
@@ -46,7 +46,8 @@ def main(debug=False):
         Maps type names from SimpleType to Java.
         """
         return {
-                'string': 'String'
+                'string': 'String',
+                'bool': 'boolean'
         }.get(s.name, s.name)
 
     def isSimpleType(s):
@@ -54,6 +55,12 @@ def main(debug=False):
         Check property type.
         """
         return isinstance(s, SimpleType)
+
+    def uncapitalize(s):
+        """
+        Change first letter to lowercase.
+        """
+        return s[0].lower() + s[1:]
 
     # Create the output folder
     srcgen_folder = join(this_folder, 'generated')
@@ -69,9 +76,14 @@ def main(debug=False):
     # Register the filter for mapping Entity type names to Java type names.
     jinja_env.filters['javatype'] = javatype
     jinja_env.filters['isSimpleType'] = isSimpleType
+    jinja_env.filters['uncapitalize'] = uncapitalize
 
-    # Load the Java template
-    template = jinja_env.get_template('entity.template')
+    # Load the Java templates
+    entity_template = jinja_env.get_template('entity.template')
+    repository_template = jinja_env.get_template('repository.template')
+    interface_template = jinja_env.get_template('interface.template')
+    service_template = jinja_env.get_template('service.template')
+    controller_template = jinja_env.get_template('controller.template')
 
     # Export to .dot file for visualization
     dot_folder = join(this_folder, 'dotexport')
@@ -91,7 +103,19 @@ def main(debug=False):
         # For each entity generate java file
         with open(join(srcgen_folder,
                       "%s.java" % entity.name.capitalize()), 'w') as f:
-            f.write(template.render(entity=entity, time=dt_string))
+            f.write(entity_template.render(entity=entity, time=dt_string))
+        with open(join(srcgen_folder,
+                      "%sRepository.java" % entity.name.capitalize()), 'w') as f:
+            f.write(repository_template.render(entity=entity, time=dt_string))
+        with open(join(srcgen_folder,
+                      "%sInterface.java" % entity.name.capitalize()), 'w') as f:
+            f.write(interface_template.render(entity=entity, time=dt_string))
+        with open(join(srcgen_folder,
+                      "%sService.java" % entity.name.capitalize()), 'w') as f:
+            f.write(service_template.render(entity=entity, time=dt_string))
+        with open(join(srcgen_folder,
+                      "%sController.java" % entity.name.capitalize()), 'w') as f:
+            f.write(controller_template.render(entity=entity, time=dt_string))
 
 
 if __name__ == "__main__":
