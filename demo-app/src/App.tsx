742303c0aa5af51d596761089494b15890a066cc
Aleksa Ivkovic <58144983+AleksaIvkovic@users.noreply.github.com>
2021-03-28 12:32:15 +0200

    Front

---
+++ demo-app/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import NavBar from './components/generated/NavBar';
+
+function App() {
+  return (
+    <React.Fragment>
+      <NavBar></NavBar>
+    </React.Fragment>
+  );
+}
+
+export default App;
---
+++ demo-app/src/components/Home/Home.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home: React.VFC = () => {
+    return(
+        <React.Fragment>
+            <h1>Welcome to your new app</h1>
+        </React.Fragment>
+    )
+}
+
+export default Home;
