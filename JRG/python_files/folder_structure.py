e4814a5398e3b0411dbc5e596247bbb7c0169d36
AleksaIvkovic <58144983+AleksaIvkovic@users.noreply.github.com>
2021-04-15 21:02:05 +0200

    Update folder_structure.py

--- JRG/python_files/folder_structure.py
+++ JRG/python_files/folder_structure.py
@@ -16,14 +16,22 @@ def create_folders(base_dir, base_name):
     #region Backend Folders
 
     folder_config.backend_folder = join(base_folder, 'demo')
-    folder_config.backend_base_folder = join(folder_config.backend_folder,'src','main','java','com','example','demo','generated')
+    folder_config.backend_base_folder = join(folder_config.backend_folder,'src','main','java','com','example','demo')
+    folder_config.backend_base_generated_folder = join(folder_config.backend_base_folder,'generated')
+
+    folder_config.backend_generated_interface_folder = join(folder_config.backend_base_generated_folder,'interfaces')
+    folder_config.backend_generated_controllers_folder = join(folder_config.backend_base_generated_folder,'controllers')
+    folder_config.backend_generated_repositories_folder = join(folder_config.backend_base_generated_folder,'repositories')
+    folder_config.backend_generated_service_folder = join(folder_config.backend_base_generated_folder,'services')
+    folder_config.backend_generated_models_folder = join(folder_config.backend_base_generated_folder,'models')
+    folder_config.backend_generated_dtos_folder = join(folder_config.backend_base_generated_folder,'dtos')
 
     folder_config.backend_interface_folder = join(folder_config.backend_base_folder,'interfaces')
-    folder_config.controllers_folder = join(folder_config.backend_base_folder,'controllers')
-    folder_config.repositories_folder = join(folder_config.backend_base_folder,'repositories')
+    folder_config.backend_controllers_folder = join(folder_config.backend_base_folder,'controllers')
+    folder_config.backend_repositories_folder = join(folder_config.backend_base_folder,'repositories')
     folder_config.backend_service_folder = join(folder_config.backend_base_folder,'services')
-    folder_config.models_folder = join(folder_config.backend_base_folder,'models')
-    folder_config.dtos_folder = join(folder_config.backend_base_folder,'dtos')
+    folder_config.backend_models_folder = join(folder_config.backend_base_folder,'models')
+    folder_config.backend_dtos_folder = join(folder_config.backend_base_folder,'dtos')
 
     #endregion
 
@@ -33,16 +41,16 @@ def create_folders(base_dir, base_name):
     folder_config.frontend_base_folder = join(folder_config.frontend_folder, 'src')
 
     folder_config.frontend_interface_folder = join(folder_config.frontend_base_folder,'interfaces')
-    folder_config.components_folder = join(folder_config.frontend_base_folder,'components')
-    folder_config.containers_folder = join(folder_config.frontend_base_folder,'containers')
+    folder_config.frontend_components_folder = join(folder_config.frontend_base_folder,'components')
+    folder_config.frontend_containers_folder = join(folder_config.frontend_base_folder,'containers')
     folder_config.frontend_service_folder = join(folder_config.frontend_base_folder,'services')
-    folder_config.types_folder = join(folder_config.frontend_base_folder,'types')
+    folder_config.frontend_types_folder = join(folder_config.frontend_base_folder,'types')
 
-    folder_config.generated_frontend_interface_folder = join(folder_config.frontend_interface_folder,'generated')
-    folder_config.generated_components_folder = join(folder_config.components_folder,'generated')
-    folder_config.generated_containers_folder = join(folder_config.containers_folder,'generated')
-    folder_config.generated_frontend_service_folder = join(folder_config.frontend_service_folder,'generated')
-    folder_config.generated_types_folder = join(folder_config.types_folder,'generated')
+    folder_config.frontend_generated_interface_folder = join(folder_config.frontend_interface_folder,'generated')
+    folder_config.frontend_generated_components_folder = join(folder_config.frontend_components_folder,'generated')
+    folder_config.frontend_generated_containers_folder = join(folder_config.frontend_containers_folder,'generated')
+    folder_config.frontend_generated_service_folder = join(folder_config.frontend_service_folder,'generated')
+    folder_config.frontend_generated_types_folder = join(folder_config.frontend_types_folder,'generated')
 
     #endregion
 
@@ -57,30 +65,56 @@ def create_folders(base_dir, base_name):
         mkdir(folder_config.backend_folder)
         copy_tree(join(this_folder, 'demo'), folder_config.backend_folder)
 
-    if not exists(folder_config.backend_base_folder):
-        mkdir(folder_config.backend_base_folder)
+    if not exists(folder_config.backend_base_generated_folder):
+        mkdir(folder_config.backend_base_generated_folder)
     else:
-        shutil.rmtree(folder_config.backend_base_folder)
-        mkdir(folder_config.backend_base_folder)
+        shutil.rmtree(folder_config.backend_base_generated_folder)
+        mkdir(folder_config.backend_base_generated_folder)
+
+    #region Generated backend
+
+    if not exists(folder_config.backend_generated_service_folder):
+        mkdir(folder_config.backend_generated_service_folder)
+
+    if not exists(folder_config.backend_generated_repositories_folder):
+        mkdir(folder_config.backend_generated_repositories_folder)
+
+    if not exists(folder_config.backend_generated_models_folder):
+        mkdir(folder_config.backend_generated_models_folder)
+
+    if not exists(folder_config.backend_generated_interface_folder):
+        mkdir(folder_config.backend_generated_interface_folder)
+
+    if not exists(folder_config.backend_generated_controllers_folder):
+        mkdir(folder_config.backend_generated_controllers_folder)
+
+    if not exists(folder_config.backend_generated_dtos_folder):
+        mkdir(folder_config.backend_generated_dtos_folder)
+    
+    #endregion
+
+    #region User backend
 
     if not exists(folder_config.backend_service_folder):
         mkdir(folder_config.backend_service_folder)
 
-    if not exists(folder_config.repositories_folder):
-        mkdir(folder_config.repositories_folder)
+    if not exists(folder_config.backend_repositories_folder):
+        mkdir(folder_config.backend_repositories_folder)
 
-    if not exists(folder_config.models_folder):
-        mkdir(folder_config.models_folder)
+    if not exists(folder_config.backend_models_folder):
+        mkdir(folder_config.backend_models_folder)
 
     if not exists(folder_config.backend_interface_folder):
         mkdir(folder_config.backend_interface_folder)
 
-    if not exists(folder_config.controllers_folder):
-        mkdir(folder_config.controllers_folder)
+    if not exists(folder_config.backend_controllers_folder):
+        mkdir(folder_config.backend_controllers_folder)
 
-    if not exists(folder_config.dtos_folder):
-        mkdir(folder_config.dtos_folder)
-    
+    if not exists(folder_config.backend_dtos_folder):
+        mkdir(folder_config.backend_dtos_folder)
+
+    #endregion
+
     #endregion
 
     #region Project/demo-app
@@ -92,46 +126,46 @@ def create_folders(base_dir, base_name):
     if not exists(folder_config.frontend_interface_folder):
         mkdir(folder_config.frontend_interface_folder)
 
-    if not exists(folder_config.generated_frontend_interface_folder):
-        mkdir(folder_config.generated_frontend_interface_folder)
+    if not exists(folder_config.frontend_generated_interface_folder):
+        mkdir(folder_config.frontend_generated_interface_folder)
     else:
-        shutil.rmtree(folder_config.generated_frontend_interface_folder)
-        mkdir(folder_config.generated_frontend_interface_folder)
+        shutil.rmtree(folder_config.frontend_generated_interface_folder)
+        mkdir(folder_config.frontend_generated_interface_folder)
 
-    if not exists(folder_config.components_folder):
-        mkdir(folder_config.components_folder)
+    if not exists(folder_config.frontend_components_folder):
+        mkdir(folder_config.frontend_components_folder)
 
-    if not exists(folder_config.generated_components_folder):
-        mkdir(folder_config.generated_components_folder)
+    if not exists(folder_config.frontend_generated_components_folder):
+        mkdir(folder_config.frontend_generated_components_folder)
     else:
-        shutil.rmtree(folder_config.generated_components_folder)
-        mkdir(folder_config.generated_components_folder)
+        shutil.rmtree(folder_config.frontend_generated_components_folder)
+        mkdir(folder_config.frontend_generated_components_folder)
 
-    if not exists(folder_config.containers_folder):
-        mkdir(folder_config.containers_folder)
+    if not exists(folder_config.frontend_containers_folder):
+        mkdir(folder_config.frontend_containers_folder)
 
-    if not exists(folder_config.generated_containers_folder):
-        mkdir(folder_config.generated_containers_folder)
+    if not exists(folder_config.frontend_generated_containers_folder):
+        mkdir(folder_config.frontend_generated_containers_folder)
     else:
-        shutil.rmtree(folder_config.generated_containers_folder)
-        mkdir(folder_config.generated_containers_folder)
+        shutil.rmtree(folder_config.frontend_generated_containers_folder)
+        mkdir(folder_config.frontend_generated_containers_folder)
 
     if not exists(folder_config.frontend_service_folder):
         mkdir(folder_config.frontend_service_folder)
 
-    if not exists(folder_config.generated_frontend_service_folder):
-        mkdir(folder_config.generated_frontend_service_folder)
+    if not exists(folder_config.frontend_generated_service_folder):
+        mkdir(folder_config.frontend_generated_service_folder)
     else:
-        shutil.rmtree(folder_config.generated_frontend_service_folder)
-        mkdir(folder_config.generated_frontend_service_folder)
+        shutil.rmtree(folder_config.frontend_generated_service_folder)
+        mkdir(folder_config.frontend_generated_service_folder)
 
-    if not exists(folder_config.types_folder):
-        mkdir(folder_config.types_folder)
+    if not exists(folder_config.frontend_types_folder):
+        mkdir(folder_config.frontend_types_folder)
 
-    if not exists(folder_config.generated_types_folder):
-        mkdir(folder_config.generated_types_folder)
+    if not exists(folder_config.frontend_generated_types_folder):
+        mkdir(folder_config.frontend_generated_types_folder)
     else:
-        shutil.rmtree(folder_config.generated_types_folder)
-        mkdir(folder_config.generated_types_folder)
+        shutil.rmtree(folder_config.frontend_generated_types_folder)
+        mkdir(folder_config.frontend_generated_types_folder)
     
     #endregion
