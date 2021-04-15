da23a94883616038ef96d00fe4bab44e7f85dc2f
AleksaIvkovic <58144983+AleksaIvkovic@users.noreply.github.com>
2021-04-15 21:01:53 +0200

    Update folder_config.py

--- JRG/python_files/folder_config.py
+++ JRG/python_files/folder_config.py
@@ -1,3 +1,9 @@
-backend_folder = backend_base_folder = backend_interface_folder = controllers_folder = repositories_folder = backend_service_folder = models_folder = dtos_folder = ''
-frontend_folder = frontend_base_folder = frontend_interface_folder = components_folder = containers_folder = frontend_service_folder = types_folder = ''
-generated_frontend_interface_folder = generated_components_folder = generated_containers_folder = generated_frontend_service_folder = generated_types_folder = ''
+backend_folder = backend_base_folder = backend_base_generated_folder = ''
+
+backend_generated_interface_folder = backend_generated_controllers_folder = backend_generated_repositories_folder = ''
+backend_generated_service_folder = backend_generated_models_folder = backend_generated_dtos_folder = ''
+
+backend_interface_folder = backend_controllers_folder = backend_repositories_folder = backend_service_folder = backend_models_folder = backend_dtos_folder = ''
+
+frontend_folder = frontend_base_folder = frontend_interface_folder = frontend_components_folder = frontend_containers_folder = frontend_service_folder = frontend_types_folder = ''
+frontend_generated_interface_folder = frontend_generated_components_folder = frontend_generated_containers_folder = frontend_generated_service_folder = frontend_generated_types_folder = ''
